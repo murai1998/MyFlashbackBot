b0ba40fbb00fcb4041575afdbd3b1e5d782d3149
Hanna Murai <hannamurai@Hannas-MBP.lan1>
2020-12-02 20:05:51 -0600

    send-simple-message

--- index.js
+++ index.js
@@ -2,28 +2,41 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN;
-
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+const dbase= {
+"new1": {"id":26, "from_id": 558626907},
+"ki": {"id":29, "from_id": 558626907}
+}
+
+bot.onText(/\/get (.+)/, (msg, match) => {
 
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  const key = match[1]; 
+if (key in dbase) {
+    const message = dbase[key]
+    console.log('key', key)
+    bot.forwardMessage(chatId, message.from_id, message.id)
+}
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+ 
 });
-
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//     // 'msg' is the received Message from Telegram
+//     // 'match' is the result of executing the regexp above on the text content
+//     // of the message
+  
+//     const chatId = msg.chat.id;
+//     const resp = match[1]; // the captured "whatever"
+  
+//     // send back the matched "whatever" to the chat
+//     bot.sendMessage(chatId, resp);
+//   });
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, JSON.stringify(msg));
 });
