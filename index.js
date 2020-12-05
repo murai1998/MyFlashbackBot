fe7e412ac1f31dbf04bd656c9b1c629b860e2ea1
Hanna Murai <hannamurai@Hannas-MacBook-Pro.local>
2020-12-05 15:15:43 -0600

    events-message

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const dbase = {
   "20-12-31": { id: 44, from_id: 558626907 },
 };
 
-// sqlite.run(`CREATE TABLE IF NOT EXISTS  flashback(
-//     id  INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT NOT NULL, from_id INTEGER NOT NULL, message_id INTEGER NOT NULL);`,function(res){
-//     if(res.error)
-//         throw res.error;
-//     console.log(res);
-// });
-// sqlite.insert("flashback",
-// {
-// key: "2020-12-03",
-// from_id: 558626907,
-// message_id: 41,
-
-// });
+sqlite.run(`CREATE TABLE IF NOT EXISTS  flashback(
+    id  INTEGER PRIMARY KEY AUTOINCREMENT, key TEXT NOT NULL, from_id INTEGER NOT NULL, message_id INTEGER NOT NULL, event TEXT NOT NULL);`,function(res){
+    if(res.error)
+        throw res.error;
+    console.log(res);
+});
+sqlite.insert("flashback",
+{
+key: "2020-12-03",
+from_id: 558626907,
+message_id: 41,
+event: "My day"
+});
 //----------------GET EVENT BY KEY---------------
 bot.onText(/\/get ([^;'\"]+)/, (msg, match) => {
   const chatId = msg.chat.id;
@@ -42,39 +42,7 @@ bot.onText(/\/get ([^;'\"]+)/, (msg, match) => {
 
 
 
-//-----------DELETE EVENT BY KEY------------------
-bot.onText(/\/delete ([^;'\"]+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const key = match[1];
-    const message = getMessage(key);
-    console.log('message', message)
-if(!message.exists){
-    bot.sendMessage(chatId, `You don't have any scheduled events on this day!`);
-    return 
-}
-if ((message.filter(x => x.from_id === msg.from.id).length == 0) && message.exists) {
-    bot.sendMessage(chatId, `You don't have any scheduled events on this day!`);
-    return 
-  }
-       
-        sqlite.delete(
-            "flashback",
-            {
-             key: key,
-             
-            },
-            function (res) {
-              if (res.error) {
-                bot.sendMessage(chatId, "Something went wrong!");
-                throw res.error;
-              } else {
-                bot.sendMessage(chatId, "Just deleted all events for this day!");
-              }
-            }
-          );
- 
-    console.log('TTEEEXXTT')
-  });
+
 
 
 
@@ -94,6 +62,7 @@ if ((message.filter(x => x.from_id === msg.from.id).length == 0) && message.exis
 
 //---------------ADD EVENT BY KEY-----------------
 const addMode = {};
+const deleteMode = {}
 bot.onText(/\/add ([^;'\"]+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const key = match[1];
@@ -101,50 +70,123 @@ bot.onText(/\/add ([^;'\"]+)/, (msg, match) => {
   var command = `What are you celebrating on this day?`;
   bot.sendMessage(chatId, command);
 });
+
+//-----------DELETE EVENT BY KEY------------------
+bot.onText(/\/delete ([^;'\"]+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const key = match[1];
+    const message = getMessage(key);
+if(!message.exists){
+    bot.sendMessage(chatId, `You don't have any scheduled events on this day!`);
+    return 
+}
+if ((message.filter(x => x.from_id === msg.from.id).length == 0) && message.exists) {
+    bot.sendMessage(chatId, `You don't have any scheduled events on this day!`);
+    return 
+  }
+  if ((message.filter(x => x.from_id === msg.from.id).length == 1) && message.exists) {
+      console.log("HERE")
+    sqlite.delete(
+        "flashback",
+        {
+         key: key,
+         from_id: msg.from.id
+        },
+        function (res) {
+          if (res.error) {
+            bot.sendMessage(chatId, "Something went wrong!");
+            throw res.error;
+          } else {
+            bot.sendMessage(chatId, "Just deleted an event!");
+          }
+        }
+      );
+  }     
+        // sqlite.delete(
+        //     "flashback",
+        //     {
+        //      key: key,
+             
+        //     },
+        //     function (res) {
+        //       if (res.error) {
+        //         bot.sendMessage(chatId, "Something went wrong!");
+        //         throw res.error;
+        //       } else {
+        //         bot.sendMessage(chatId, "Just deleted all events for this day!");
+        //       }
+        //     }
+        //   );
+ else{
+    deleteMode[chatId] = { key: key, from: msg.from.id, id: msg.id };
+    bot.sendMessage(chatId, "You have several scheduled events for this day:");
+    var my_events = []
+    var i = 0;
+    message.forEach((x) => {
+        if(x.from_id === msg.from.id){
+            my_events.push(x)
+            my_events[i].index = i + 1;
+            i++;
+        } 
+    })
+
+  my_events.forEach((x) => {
+        bot.forwardMessage(chatId, x.from_id, x.message_id);
+      });
+      bot.sendMessage(chatId, `Reply with an index of the event you want to delete, if you want to remove all of them, reply "all"`);
+ }
+
+  });
 //-----------TYPE MESSAGE------------
 bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  if (!(chatId in addMode)) {
-    return;
-  }
-  const row = addMode[chatId];
-  sqlite.insert(
-    "flashback",
-    {
-      key: row.key,
-      from_id: row.from,
-      message_id: msg.message_id,
-    },
-    function (res) {
-      if (res.error) {
-        bot.sendMessage(chatId, "Something went wrong!");
-        throw res.error;
-      } else {
-        bot.sendMessage(chatId, "Just added this event!");
-      }
+    const chatId = msg.chat.id;
+    if(chatId in deleteMode) {
+        console.log(msg)
     }
-  );
-  delete addMode[chatId];
-});
-
-//------------SHOW ALL EVENTS----------------
-bot.onText(/\/events/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const fromId = msg.from.id;
-  const data = sqlite.run("SELECT * FROM flashback WHERE `from_id` = ?", [
-    fromId,
-  ]);
-  if (data.length == 0) {
-    bot.sendMessage(chatId, "Your event calendar is still empty");
-    return;
-  }
-  var events = [];
-  data.forEach((x) => {
-      if(!(events.includes(x.key)))
-    events.push(`${x.key}`);
+    if (!(chatId in addMode)) {
+      return;
+    }
+    const row = addMode[chatId];
+    sqlite.insert(
+      "flashback",
+      {
+        key: row.key,
+        from_id: row.from,
+        message_id: msg.message_id,
+        event: msg.text
+      },
+      function (res) {
+        if (res.error) {
+          bot.sendMessage(chatId, "Something went wrong!");
+          throw res.error;
+        } else {
+          bot.sendMessage(chatId, "Just added this event!");
+        }
+      }
+    );
+ 
+    delete addMode[chatId];
+   
   });
-  bot.sendMessage(chatId, events.join("\n"), { parse_mode: "markdown" });
-});
+  
+  //------------SHOW ALL EVENTS----------------
+  bot.onText(/\/events/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const fromId = msg.from.id;
+    const data = sqlite.run("SELECT * FROM flashback WHERE `from_id` = ?", [
+      fromId,
+    ]);
+    if (data.length == 0) {
+      bot.sendMessage(chatId, "Your event calendar is still empty");
+      return;
+    }
+    var events = [];
+    data.forEach((x) => {
+        if(!(events.includes(x.key)))
+      events.push(`${x.key}`);
+    });
+    bot.sendMessage(chatId, events.join("\n"), { parse_mode: "markdown" });
+  });
 
 function isMessage(key) {
   return (
@@ -156,7 +198,6 @@ function isMessage(key) {
 //-------------EXTRACTING EVENT FROM THE DATABASE---------------
 function getMessage(key) {
   const data = sqlite.run("SELECT * FROM flashback WHERE `key` = ? ", [key]);
-  console.log("data", data);
   if (data.length == 0) {
     return { exists: false };
   }
